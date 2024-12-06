e75d7d48422c04548926a2307bb7f0ba4f1cb388
jgisprogramming <jghan.2022@scis.smu.edu.sg>
2024-12-06 13:04:01 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue']
   },
   base: '/jgprogrammingbrain.github.io/' // Changed to relative path
 })
